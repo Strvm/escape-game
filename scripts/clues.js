8dabe4cd3250a142899b66394d2130f0a1312fd8
Strum <phillipsromeo@gmail.com>
2020-04-24 05:34:09 +0200

    Latest version

--- scripts/clues.js
+++ scripts/clues.js
@@ -1,236 +1,274 @@
-"use strict"
+"use strict" 
 {
-let { rooms,throwError , getCurrentRoom, currentRoom, currentLevel, getNextRoom, getRoomId, goToRoom, getMaxRooms, isRoomValid} = ESCAPE_ROOM;
-Object.assign(ESCAPE_ROOM, {setFoundKey, typeWriter});
-let clueName;
-let foundKey = false;
-let lastInteraction = '';
-let foundHideSpot = false;
-let isLocked = true;
+    let {
+        rooms,
+        throwError,
+        getCurrentRoom,
+        currentRoom,
+        currentLevel,
+        getNextRoom,
+        getRoomId,
+        goToRoom,
+        getMaxRooms,
+        isRoomValid
+    } = ESCAPE_ROOM;
+    Object.assign(ESCAPE_ROOM, {
+        setFoundKey,
+        typeWriter,
+        playSound
+    });
+    let clueName;
+    let foundKey = false;
+    let lastInteraction = '';
+    let foundHideSpot = false;
+    let isLocked = true;
+    let tireBouchonFound = false;
+    let isOpen = false;
 
-const clueClick = (event) => {
-   if(!isClue(event.target.className))return;
-   clueName = getClueName(event.target.className);
-   changeFrame(clueName);
-   lastInteraction = clueName;
-}
-
-const next = (event) => {
-    console.log('test');
-    goToRoom(getNextRoom(getCurrentRoom()));
- }
- 
-function setFoundKey(test){
-    foundKey = test;
-}
-
-function getClueName(clueName){
-    return clueName.replace('clue ', '');
-}
-
-function isClue(clueName){
-    if(clueName.includes('clue'))return true;
-    return false;
-}
-
-function isKey(clueName){
-    if(clueName == 'key'){
-        return true;
+    const clueClick = (event) => {
+        if (!isClue(event.target.className)) return;
+        clueName = getClueName(event.target.className);
+        changeFrame(clueName);
+        lastInteraction = clueName;
     }
-    return false;
-}
 
-function playSound(sound){    
-    const audio = new Audio(`../sounds/${sound}`);
-    audio.onerror = function(){ // Failed to load
-        throwError(`Failed to play find sound at: ${audio.src}`);
+
+    function setFoundKey(test) {
+        foundKey = test;
+    }
+
+    function getClueName(clueName) {
+        return clueName.replace('clue ', '');
+    }
+
+    function isClue(clueName) {
+        if (clueName.includes('clue')) return true;
         return false;
-    };
-    audio.volume = 0.5;
-    audio.play();
-}
-
-function setFrame(){
-
-}
-
-function changeFrame(clueName){
-    const level =  document.querySelector(`.level-${getRoomId(getCurrentRoom())}`)    
-    const img = new Image();
-
-    if (clueName == 'door') {
-        if(foundKey){
-            foundKey = false;
-            isLocked = true;
-            lastInteraction = '';
-            playSound('key-open.mp3');
-            setTimeout(function(){goToRoom(getNextRoom(getCurrentRoom()))},2000)
-            document.querySelector('.key-stuff').style.display = "none";
-            document.querySelector('.key-success').style.display = "none"; 
-
-            document.querySelector('.tirebouchon-stuff').style.display = "none";
-            document.querySelector(".tirebouchon-success").style.display = "none";
-
-            return;
-        }
-        return;
     }
 
-    if (lastInteraction == clueName && lastInteraction != 'coffreOuvert') {
-        if (foundKey && clueName && getCurrentRoom() == 'room level-2') {            
-            img.src = `../images/frames/level-${getRoomId(getCurrentRoom())}/level-${getRoomId(getCurrentRoom())}-nokey.svg`;
-        }else{
-            img.src = `../images/frames/level-${getRoomId(getCurrentRoom())}/level-${getRoomId(getCurrentRoom())}.svg`;
+    function isKey(clueName) {
+        if (clueName == 'key') {
+            return true;
         }
-        img.onload = function(){             
-            level.style.backgroundImage = `url("${img.src}")`;
-            lastInteraction = '';
-        }
-        return;
+        return false;
     }
-    console.log(clueName);
-    
-    if (foundKey && getCurrentRoom() == 'room level-2'){
-        img.src = `../images/frames/level-${getRoomId(getCurrentRoom())}/${clueName}-nokey.svg`;
-    }else{
-        img.src = `../images/frames/level-${getRoomId(getCurrentRoom())}/${clueName}.svg`;
-    }
-    if (isKey(clueName)) {
-        if(!foundKey && lastInteraction == 'keyHide'){
-            foundKey = true;
-            document.querySelector('.key').style.display = 'none';
-            playSound('pick-key.mp3');
-            document.querySelector('.key-stuff').style.display = "block"; 
-            document.querySelector('.key-success').style.display = "block"; 
-            img.src = `../images/frames/level-${getRoomId(getCurrentRoom())}/level-${getRoomId(getCurrentRoom())}-nokey.svg`;
-            img.onload = function(){ 
-            level.style.backgroundImage = `url("${img.src}")`;
-            document.querySelector(`.level-${getRoomId(getCurrentRoom())} .keyHide`).style.zIndex = '10';
-            return;
-            }
-        }else{
-            document.querySelector(`.level-${getRoomId(getCurrentRoom())} .keyHide`).style.zIndex = '10'; 
-        }
-    }else{
-        img.onload = function(){ 
-            switch(getCurrentRoom()){
-                case 'room level-1':
-                    if (getCurrentRoom() == 'room level-1') {    
-                        if (foundHideSpot) {
-                            foundHideSpot = false;
-                            document.querySelector(`.level-${getRoomId(getCurrentRoom())} .keyHide`).style.zIndex = '10';
-                        }
-                        if (clueName == 'keyHide') { 
-                            foundHideSpot = true;
-                            document.querySelector(`.level-${getRoomId(getCurrentRoom())} .${clueName}`).style.zIndex = '5';
-                        }
-        
-                    }
-                    break;
-                case 'room level-2':
-                    if (clueName == 'tireBouchon' && isLocked) {
-                        isLocked = false;
-                        document.querySelector(".tirebouchon-stuff").style.display = "block"
-                        document.querySelector(".tirebouchon-success").style.display = "block"
 
-                    }
-                    break;
-                case 'room level-3':
-                    if (clueName == 'coffreOuvert') {
-                        document.querySelector(`.safeVaultContainer`).style.display = 'block';
-                    }
-                    break;
-                case 'room level-4':
-                    if (clueName == 'placard') {
-                        document.querySelector(`.labyrinthContainer`).style.display = 'block';
-                    }
-                    break;
-            }
-            if(getCurrentRoom() != 'room level-1'){
-                if (foundHideSpot) {
-                    foundHideSpot = false;
-                    document.querySelector(`.level-${getRoomId(getCurrentRoom())} .keyHide`).style.zIndex = '10';
+    function playSound(sound) {
+        const audio = new Audio(`../sounds/${sound}`);
+        audio.onerror = function() { // Failed to load
+            throwError(`Failed to play find sound at: ${audio.src}`);
+            return false;
+        };
+        audio.volume = 0.5;
+        audio.play();
+    }
+
+    function clearText(){
+        try {
+            const lastDialogue = document.querySelector(lastText);
+            lastDialogue.classList.add('hidden');
+            lastDialogue.textContent = savedText;
+            clearTimeout(timeOut);
+
+        } catch (error) {}   
+    }
+
+    function changeFrame(clueName) {
+        const level = document.querySelector(`.level-${getRoomId(getCurrentRoom())}`)
+        const img = new Image();
+
+        if (clueName == 'door') {
+            if (foundKey) {
+                if(isOpen){
+                    foundKey = false;
+                    isLocked = true;
+                    isOpen = false;
+                    lastInteraction = '';
+                    goToRoom(getNextRoom(getCurrentRoom()))
+                    document.querySelector('.key-stuff').style.display = "none";
+                    document.querySelector('.key-success').style.display = "none";
+                    document.querySelector('.tirebouchon-stuff').style.display = "none";
+                    document.querySelector(".tirebouchon-success").style.display = "none";
+                }else{
+                    playSound('key-open.mp3');
+                    setTimeout(function() {
+                        isOpen = true;
+                        img.src = `../images/frames/level-${getRoomId(getCurrentRoom())}/level-${getRoomId(getCurrentRoom())}-opendoor.svg`;
+                        img.onload = function() {
+                        level.style.backgroundImage = `url("${img.src}")`;
+                        clearText();                          
+                        typeWriter(`.dialogues-zone .doorOpen${getRoomId(getCurrentRoom())+1}`, false, 50);
+                        playSound('door-open.mp3');
                 }
-
-                if (clueName == 'keyHide' && isLocked) {  
-                    return;
-                }
-
-                if (clueName == 'keyHide' && !isLocked) {  
-                    foundHideSpot = true;
-                    document.querySelector(`.level-${getRoomId(getCurrentRoom())} .key`).style.display = 'block';
-                    document.querySelector(`.level-${getRoomId(getCurrentRoom())} .${clueName}`).style.zIndex = '5';
+                    }, 750)
                 }
                 
+                return;
             }
-            try {                
-                const lastDialogue = document.querySelector(lastText);
-                lastDialogue.classList.add('hidden');
-                lastDialogue.textContent = savedText;
-                clearTimeout(timeOut);
-            } catch (error) {
-            }
-            typeWriter(`.dialogues-zone .${clueName}`, false, 50)
-            level.style.backgroundImage = `url("${img.src}")`;
-            }
+            return;
+        }
 
-            
-        
-        if(clueName == 'keyHide' && foundKey && getCurrentRoom() == 'room level-1'){
-            img.src = `../images/frames/level-${getRoomId(getCurrentRoom())}/level-${getRoomId(getCurrentRoom())}-nokey.svg`;
-            img.onload = function(){ 
+        if (lastInteraction == clueName && lastInteraction != 'coffreOuvert') {
+            if (foundKey && clueName && getCurrentRoom() == 'room level-2') {
+                img.src = `../images/frames/level-${getRoomId(getCurrentRoom())}/level-${getRoomId(getCurrentRoom())}-nokey.svg`;
+            } else {
+                img.src = `../images/frames/level-${getRoomId(getCurrentRoom())}/level-${getRoomId(getCurrentRoom())}.svg`;
+            }
+            img.onload = function() {
+                level.style.backgroundImage = `url("${img.src}")`;
+                lastInteraction = '';
+            }
+            return;
+        }
+        if (foundKey && getCurrentRoom() == 'room level-2') {
+            img.src = `../images/frames/level-${getRoomId(getCurrentRoom())}/${clueName}-nokey.svg`;
+        } else {
+            img.src = `../images/frames/level-${getRoomId(getCurrentRoom())}/${clueName}.svg`;
+        }
+        if (isKey(clueName)) {
+
+            if (!foundKey && lastInteraction == 'keyHide') {
+                foundKey = true;
+                document.querySelector('.key').style.display = 'none';
+                playSound('pick-key.mp3');
+                document.querySelector('.key-stuff').style.display = "block";
+                document.querySelector('.key-success').style.display = "block";
+                img.src = `../images/frames/level-${getRoomId(getCurrentRoom())}/level-${getRoomId(getCurrentRoom())}-nokey.svg`;
+                img.onload = function() {
+                    level.style.backgroundImage = `url("${img.src}")`;
+                    document.querySelector(`.level-${getRoomId(getCurrentRoom())} .keyHide`).style.zIndex = '10';
+                    typeWriter(`.dialogues-zone .keyFound`, false, 50);
+                    return;
+                }
+            } else {
+                document.querySelector(`.level-${getRoomId(getCurrentRoom())} .keyHide`).style.zIndex = '10';
+            }
+        } else {
+            img.onload = function() {
+                switch (getCurrentRoom()) {
+                    case 'room level-1':
+                        if (getCurrentRoom() == 'room level-1') {
+                            if (foundHideSpot) {
+                                foundHideSpot = false;
+                                document.querySelector(`.level-${getRoomId(getCurrentRoom())} .keyHide`).style.zIndex = '10';
+                            }
+                            if (clueName == 'keyHide') {
+                                foundHideSpot = true;
+                                document.querySelector(`.level-${getRoomId(getCurrentRoom())} .${clueName}`).style.zIndex = '5';
+                            }
+
+                        }
+                        break;
+                    case 'room level-2':
+                        if (clueName == 'tiroirTireBouchon' && !tireBouchonFound) {
+                            tireBouchonFound = true;
+                            document.querySelector('.tireBouchon').style.display = 'block';
+                        }
+                        if (clueName == 'tireBouchon' && isLocked) {
+                            isLocked = false;
+                            document.querySelector('.tireBouchon').style.display = 'none';
+                            document.querySelector(".tirebouchon-stuff").style.display = "block"
+                            document.querySelector(".tirebouchon-success").style.display = "block"
+                        }
+                        break;
+                    case 'room level-3':
+                        if (clueName == 'coffreOuvert') {
+                            document.querySelector(`.safeVaultContainer`).style.display = 'block';
+                        }
+                        break;
+                    case 'room level-4':
+                        if (clueName == 'placard') {
+                            document.querySelector(`.labyrinthContainer`).style.display = 'block';
+                        }
+                        break;
+                }
+                if (getCurrentRoom() != 'room level-1') {
+                    if (foundHideSpot) {
+                        foundHideSpot = false;
+                        document.querySelector(`.level-${getRoomId(getCurrentRoom())} .keyHide`).style.zIndex = '10';
+                    }
+
+                    if (clueName == 'keyHide' && isLocked) {
+                        return;
+                    }
+
+                    if (clueName == 'keyHide' && !isLocked) {
+                        foundHideSpot = true;
+                        document.querySelector(`.level-${getRoomId(getCurrentRoom())} .key`).style.display = 'block';
+                        document.querySelector(`.level-${getRoomId(getCurrentRoom())} .${clueName}`).style.zIndex = '5';
+                    }
+
+                }
+                clearText();             
+                typeWriter(`.dialogues-zone .${clueName}`, false, 50)
                 level.style.backgroundImage = `url("${img.src}")`;
             }
+
+
+
+            if (clueName == 'keyHide' && foundKey && getCurrentRoom() == 'room level-1') {
+                img.src = `../images/frames/level-${getRoomId(getCurrentRoom())}/level-${getRoomId(getCurrentRoom())}-nokey.svg`;
+                img.onload = function() {
+                    level.style.backgroundImage = `url("${img.src}")`;
+                }
+            }
+
+            if (clueName == 'tiroirTireBouchon' && tireBouchonFound && getCurrentRoom() == 'room level-2') {
+                img.src = `../images/frames/level-${getRoomId(getCurrentRoom())}/tiroirTireBouchon-notir.svg`;
+                img.onload = function() {
+                    level.style.backgroundImage = `url("${img.src}")`;
+                }
+            }
+        }
+
+    }
+
+
+
+
+    document.getElementById('rooms').addEventListener('click', clueClick);
+
+
+    let i = 0; //Our counter for the loop.
+    let text; //Store the element.
+    let textChars; //Stores the characters before they get removed.
+    let timeOut; //Stores the setTimeOut so we can cancel it when we want.
+    let timeOut2;
+    let lastText = '' //Stores the last element printed.
+    let savedText = ''; //Stores the last text contents printed.
+    function typeWriter(textid, wipe, speed) {
+        textid = textid.split('text')[0] //Format the ID of the element we want to print.
+        text = document.querySelector(textid);
+        if (text == null) {
+            console.log('Given paragraph ID is null.');
+            return;
+        }
+        if (!wipe) { //This condition is only ran the first time you execute the function.
+            i = 0;
+            clearTimeout(timeOut2)
+            savedText = text.textContent;
+            lastText = textid;
+            textChars = text.textContent;
+            text.textContent = "";
+            text.style.display = 'block' //Display the text container we want to print.
+            wipe = true;
+        }
+        text.classList.remove('hidden'); //Make the text visible
+        if (i < textChars.length) {
+            text.textContent += textChars.charAt(i); //Adding new character to the content of the span     
+            i++;
+            timeOut2 = setTimeout(function() {
+                typeWriter('.dialogues-zone .' + text.className, wipe, speed);
+            }, speed);
+        } else {
+            timeOut = setTimeout(function() {
+                if (!text.className.includes('hidden')) text.classList.add('hidden');
+            }, 4000); //Dissappear after 4s
+            i = 0;
         }
     }
 
-}
-
-
-
-document.querySelector('.nextRoom').addEventListener('click', next);
-
-document.getElementById('rooms').addEventListener('click', clueClick);
-
-
-let i = 0; //Our counter for the loop.
-let text; //Store the element.
-let textChars; //Stores the characters before they get removed.
-let timeOut; //Stores the setTimeOut so we can cancel it when we want.
-let lastText = ''//Stores the last element printed.
-let savedText = '';//Stores the last text contents printed.
-function typeWriter(textid, wipe, speed) {    
-    textid = textid.split('text')[0] //Format the ID of the element we want to print.
-    text = document.querySelector(textid);
-    if (text == null){
-        console.log('Given paragraph ID is null.');
-        return;
-    }
-    if (!wipe){//This condition is only ran the first time you execute the function.
-        savedText = text.textContent;
-        lastText = textid;
-        textChars = text.textContent;
-        text.textContent = "";
-        text.style.display = 'block'//Display the text container we want to print.
-      	wipe = true;
-    }
-    text.classList.remove('hidden');//Make the text visible
-    if (i < textChars.length) {
-      text.textContent += textChars.charAt(i); //Adding new character to the content of the span     
-      i++;
-      setTimeout(function(){ typeWriter('.dialogues-zone .' + text.className, wipe, speed); }, speed);
-    }else{
-        timeOut = setTimeout(function(){ text.classList.add('hidden');},4000);//Dissappear after 4s
-        textChars = null;
-        text = null;
-        i = 0;
-    }
-  }
-
-
 
 
 
 }
-
-
--- scripts/safeVault.js
+++ scripts/safeVault.js
@@ -1,72 +1,72 @@
-
-"use strict"
+"use strict" 
 {
-const { setFoundKey, getRoomId, getCurrentRoom} = ESCAPE_ROOM;
-const pin = 810
-let enterCode = '';
-let isUnlocked = false;
+    const {
+        setFoundKey,
+        getRoomId,
+        getCurrentRoom,
+        playSound
+    } = ESCAPE_ROOM;
+    const pin = 810
+    let enterCode = '';
+    let isUnlocked = false;
 
-document.querySelector('.pincode').addEventListener('click', (event) => {
-	if (event.target.className.includes('button')){
-		const clickedNumber = event.target.innerText;
-		
-		console.log(clickedNumber)
-		
-		enterCode = enterCode + clickedNumber
-		let lengthCode = enterCode.length
-		lengthCode--
-		document.querySelector(`.fields .c${lengthCode} .numberfield`).classList.add('active');
-		document.querySelector('.textContainer .codeText').innerHTML = `<strong>Il faut entrer un code...</strong>`;
-		if (lengthCode == 2) {
-			// Check if the code is correct.
-			if (enterCode == pin) {
-
-			// If the code is correct, do this :
-			document.querySelector(`.fields .c${lengthCode} .numberfield`).classList.add('active');
-			document.querySelector('.numbers').classList.add('hide');
-			document.querySelector('.keyWin').style.display = 'block';
-			document.querySelector('.textContainer .codeText').display = 'none';
-			document.querySelector('.textContainer .codeText').innerHTML = ``
-			isUnlocked = true;
-			setTimeout(function () {
-				document.querySelector(`.safeVaultContainer`).style.display = 'none';
-			}, 1000)
-			setFoundKey(true);
-			document.querySelector('.key-stuff').style.display = "block";
-			document.querySelector('.key-success').style.display = "block"; 
-
-			} 
-			else {
-			// If the code entered is wrong.
-			enterCode = '';
-			document.querySelector('.fields').classList.add('miss')
-			document.querySelector(`.fields .c${lengthCode} .numberfield`).classList.remove('right')
-			document.querySelector(`.fields .c0 .numberfield`).classList.remove('active')
-			document.querySelector(`.fields .c1 .numberfield`).classList.remove('active')
-			document.querySelector(`.fields .c2 .numberfield`).classList.remove('active')
-			document.querySelector('.numbers').classList.remove('hide')
-			document.querySelector('.textContainer .codeText').innerHTML = `<strong>Vous n'avez pas trouvé le bon code... !</strong>`
-
-			setTimeout(function () {
-				document.querySelector(`.fields .c${lengthCode} .numberfield`).classList.remove('active')
-				}, 200)
-				setTimeout(function () {
-				document.querySelector('.fields').classList.remove('miss')
-				}, 500)
-			}
-		} 
-	}else if(event.target.className.includes('close')){
-		const level =  document.querySelector(`.level-${getRoomId(getCurrentRoom())}`) 
-		const img = new Image();
-		document.querySelector(`.safeVaultContainer`).style.display = 'none';
-		if(!isUnlocked)
-			img.src = img.src = `../images/frames/level-${getRoomId(getCurrentRoom())}/level-${getRoomId(getCurrentRoom())}.svg`;
-		else
-			img.src = img.src = `../images/frames/level-${getRoomId(getCurrentRoom())}/coffreOuvert.svg`;
-		img.onload = function(){ 
-			level.style.backgroundImage = `url("${img.src}")`;
-		}
-	}
-});
+    document.querySelector('.pincode').addEventListener('click', (event) => {
+        if (event.target.className.includes('button')) {
+            const clickedNumber = event.target.innerText;
+            playSound('click.mp3')
+            enterCode = enterCode + clickedNumber
+            let lengthCode = enterCode.length
+            lengthCode--
+            document.querySelector(`.fields .c${lengthCode} .numberfield`).classList.add('active');
+            document.querySelector('.textContainer .codeText').innerHTML = `<strong>Il faut entrer un code...</strong>`;
+            if (lengthCode == 2) {
+                // Check if the code is correct.
+                if (enterCode == pin) {
+                    // If the code is correct, do this :
+                    document.querySelector(`.fields .c${lengthCode} .numberfield`).classList.add('active');
+                    document.querySelector('.numbers').classList.add('hide');
+                    document.querySelector('.keyWin').style.display = 'block';
+                    document.querySelector('.textContainer .codeText').display = 'none';
+                    document.querySelector('.textContainer .codeText').innerHTML = ``
+                    isUnlocked = true;
+                    setTimeout(function() {
+                        document.querySelector(`.safeVaultContainer`).style.display = 'none';
+                    }, 1000)
+                    setFoundKey(true);
+                    document.querySelector('.key-stuff').style.display = "block";
+                    document.querySelector('.key-success').style.display = "block";
+                    playSound('beep.mp3')
+                } else {
+                    // If the code entered is wrong.
+                    enterCode = '';
+                    document.querySelector('.fields').classList.add('miss')
+                    document.querySelector(`.fields .c${lengthCode} .numberfield`).classList.remove('right')
+                    document.querySelector(`.fields .c0 .numberfield`).classList.remove('active')
+                    document.querySelector(`.fields .c1 .numberfield`).classList.remove('active')
+                    document.querySelector(`.fields .c2 .numberfield`).classList.remove('active')
+                    document.querySelector('.numbers').classList.remove('hide')
+                    document.querySelector('.textContainer .codeText').innerHTML = `<strong>Vous n'avez pas trouvé le bon code... !</strong>`
+                    playSound('error.mp3')
+                    setTimeout(function() {
+                        document.querySelector(`.fields .c${lengthCode} .numberfield`).classList.remove('active')
+                    }, 200)
+                    setTimeout(function() {
+                        document.querySelector('.fields').classList.remove('miss')
+                    }, 500)
+                }
+            }
+        } else if (event.target.className.includes('close')) {
+            const level = document.querySelector(`.level-${getRoomId(getCurrentRoom())}`)
+            const img = new Image();
+            document.querySelector(`.safeVaultContainer`).style.display = 'none';
+            if (!isUnlocked)
+                img.src = img.src = `../images/frames/level-${getRoomId(getCurrentRoom())}/level-${getRoomId(getCurrentRoom())}.svg`;
+            else
+                img.src = img.src = `../images/frames/level-${getRoomId(getCurrentRoom())}/coffreOuvert.svg`;
+            img.onload = function() {
+                level.style.backgroundImage = `url("${img.src}")`;
+            }
+        }
+    });
 
 }
